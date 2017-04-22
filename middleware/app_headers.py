2622ce3883ea9d70694101e0559f14f64619ac94
hnroot <root@unicast.tv>
2017-04-22 14:00:55 +0300

    AppHeadersMiddleware is added to middleware application.

--- middleware/app_headers.py
+++ middleware/app_headers.py
@@ -1,1 +1,43 @@
 # -*- coding: utf-8 -*-
+
+
+class AppHeadersMiddleware(object):
+
+    def process_request(self, request):
+
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth_elements = request.META['HTTP_AUTHORIZATION'].split()
+
+            if len(auth_elements) == 2 and auth_elements[0].lower() == 'token':
+                request.auth_token = auth_elements[1]
+            else:
+                request.auth_token = None
+        else:
+            request.auth_token = None
+
+        if 'HTTP_APPLICATIONCONTEXT' in request.META:
+            request.app_context = request.META['HTTP_APPLICATIONCONTEXT']
+        else:
+            request.app_context = None
+
+        if 'HTTP_APPVERSION' in request.META:
+            request.app_version = request.META['HTTP_APPVERSION']
+        else:
+            request.app_version = None
+
+        if 'HTTP_DEVICETYPE' in request.META:
+            request.device_type = request.META['HTTP_DEVICETYPE']
+        else:
+            request.device_type = None
+
+        if 'HTTP_DEVICETOKEN' in request.META:
+            request.device_token = request.META['HTTP_DEVICETOKEN']
+        else:
+            request.device_token = None
+
+        if 'HTTP_BUILDCONFIG' in request.META:
+            request.build_config = request.META['HTTP_BUILDCONFIG']
+        else:
+            request.build_config = None
+
+        return None
